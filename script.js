7eee432519ae099697feac4d0cc30819bc2f940e
Galym <galymsakitzhan@gmail.com>
2023-12-16 01:08:09 +0600

    add date for timer

--- script.js
+++ script.js
@@ -2,24 +2,31 @@
 const start = document.getElementById('start');
 let stop = document.querySelector('#stop');
 let resert = document.querySelector('#resert');
-let timer = document.querySelector('.timer');
+let timer = document.querySelector('#timer');
 
 
 start.addEventListener('click', () =>{
-    console.log("Kotakbas");
-    start.style.backgroundColor = 'black';
-    start.style.color = 'white';
+    start.classList.toggle('blueshow');
 })
 
 
 stop.addEventListener('click', () =>{
-    console.log("Kotakbas");
-    stop.style.backgroundColor = 'black';
-    stop.style.color = 'white';
+    stop.classList.toggle('redshow')
 })
 
-resert.addEventListener('click', () =>{
-    console.log("Kotakbas");
-    resert.style.backgroundColor = 'black';
-    resert.style.color = 'white';
-})
+
+function updateTimer(){
+    future = Date.parse("jun 12, 2022 01:30:00");
+    now = Date.parse("jun 12, 2022 01:00:00");
+    diff = future - now;
+
+    mins = Math.floor(diff / (1000 * 60));
+    secs = Math.floor(diff / 1000);
+
+    s = secs - mins * 60;
+
+    timer.innerHTML = + " " + mins + ":" + s 
+
+}
+
+updateTimer();
