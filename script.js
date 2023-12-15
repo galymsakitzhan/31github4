6b19c1129105b153e0a531a0128d65b1ab5a798a
Galym <galymsakitzhan@gmail.com>
2023-12-16 00:31:10 +0600

    add buttons function

---
+++ script.js
@@ -0,0 +1,25 @@
+
+const start = document.getElementById('start');
+let stop = document.querySelector('#stop');
+let resert = document.querySelector('#resert');
+let timer = document.querySelector('.timer');
+
+
+start.addEventListener('click', () =>{
+    console.log("Kotakbas");
+    start.style.backgroundColor = 'black';
+    start.style.color = 'white';
+})
+
+
+stop.addEventListener('click', () =>{
+    console.log("Kotakbas");
+    stop.style.backgroundColor = 'black';
+    stop.style.color = 'white';
+})
+
+resert.addEventListener('click', () =>{
+    console.log("Kotakbas");
+    resert.style.backgroundColor = 'black';
+    resert.style.color = 'white';
+})
